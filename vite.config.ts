df424c9e21f0b5c7908067e9a281dc7511632ce5
Coders in Flow <codersinflow@localhost>
2026-05-01 10:43:13 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/data-app': {
+        target: 'http://localhost:8001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/data-app/, ''),
+      },
+    },
+  },
 })
